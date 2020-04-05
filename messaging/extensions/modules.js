b3a7ee062dc3c22c291f089e60f4dea4a4e8a580
ThuF <jordan.p.pavlov@gmail.com>
2020-04-05 17:57:02 +0300

    Adds "api-modules" Extension

---
+++ messaging/extensions/modules.js
@@ -0,0 +1,9 @@
+exports.getContent = function() {
+	return [{
+		name: "messaging/v4/consumer",
+		description: "Messaging Consumer API"
+	}, {
+		name: "messaging/v4/producer",
+		description: "Messaging Producer API"
+	}];
+};
